667dadd2e6c9edfe378da6d1a6445b7065062c7e
Tara Sabella <tarasabella@gmail.com>
2018-11-17 19:52:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,5 +10,11 @@ end
   
 end
 
-def countdown_with_sleep(number)
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0 
+  puts "#{seconds_to_midnight} SECOND(S)!"
+  seconds_to_midnight -= 1 
+end
+   "HAPPY NEW YEAR!"
+end
 
